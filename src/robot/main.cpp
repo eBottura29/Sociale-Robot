27bd1d64b031228737348ff4c8dade4d3301bd17
eBottura29 <botturaenrico@gmail.com>
2026-01-09 19:57:49 +0100

    main.cpp init, nog niets interesants in de main.cpp

---
+++ src/robot/main.cpp
@@ -0,0 +1,15 @@
+// TODO: ik ga lib later fixen trust
+
+// #include <Wire.h>
+// #include <lib/Dwenguino/Dwenguino.h>
+// #include <LiquidCrystal.h>
+
+// void setup() {
+//     initDwenguino();
+//     // setup
+// }
+
+
+// void loop() {
+//     // loop
+// }
